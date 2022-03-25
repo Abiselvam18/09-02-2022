73bc9385bb0ffee86910a736a2ee7b9d3e6732a9
Abinaya <abiselvam080@gmail.com>
2022-03-25 20:07:43 +0530

    commit five

---
+++ 09-02-2022/src/main/java/com/mavenproject/_02_2022/Example4.java
@@ -0,0 +1,14 @@
+package com.mavenproject._02_2022;
+
+public class Example4 {
+
+	public static void main(String[] args) {
+	
+		int a=18, b=8, sum;
+		sum=a+b;
+		System.out.println("sum of numbers="+sum);
+	
+
+	}
+
+}
